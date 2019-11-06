753fce4dd8c7b5610a24e6ad04b0ff673ab7089a
hristijan12 <49168243+hristijan12@users.noreply.github.com>
2019-11-06 22:06:45 +0100

    11.06 - cas2

---
+++ index.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://dev:********@cluster0-or99i.mongodb.net:27017/videoteka', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(res => {
+        console.log(rest);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+    const Filmovi = mongoose.model( 
+        "Filmovi",
+        {
+            ime: String,
+            godina: Date,
+            zanr: [String],
+            rezija: String,
+            oscar: Boolean,
+            akteri: [String]
+        }
+    );
+
+    Filmovi.find((err, data) => {
+        if(err){
+            console.log('ERROR');
+        }
+        console.log(data);
+    })
+    console.log('end!');
