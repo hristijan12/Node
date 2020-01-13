3639f19563c15a888d49eed9113de7536d222665
hristijan12 <hristijan.taseski@yahoo.com>
2020-01-13 20:32:06 +0100

    13.01

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,5 +1,5 @@
-const mUsers = require('../models/users')
-const vUsers = require('../validators/users')
+const mUsers = require('../models/users');
+const vUsers = require('../validators/users');
 var validator = require('node-input-validator');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
@@ -7,68 +7,76 @@ const config = require('../config/index.js');
 const randomstring = require('randomstring');
 const sgMail = require('@sendgrid/mail');
 
-
-
 const register = (req, res) => {
-    var v = new validator.Validator(req.body, vUsers.createUser)
+    var v = new validator.Validator(req.body, vUsers.createUser);
     v.check()
     .then(matched => {
         if(matched) {
-            bcrypt.genSalt(10, function(err, salt) {
-                if(err){
-                    throw new Error (err);
-                    return;
-                }    
-                bcrypt.hash(req.body.password, salt, function(err, hash) {
-                    if(err){
-                        throw new Error(err);
-                        return;
-                    }
-                    var confirm_hash = randomstring.generate({
-                        length: 30,
-                        charset: 'alphanumeric'
+            // get user by email
+            return mUsers.getUserPasswordByEmail(req.body.email)
+            .then((ed) => {
+                console.log(ed);
+                if(!ed) {
+                    bcrypt.genSalt(10, function(err, salt) {
+                        if(err){
+                            throw new Error(err);
+                            return;
+                        }
+                        bcrypt.hash(req.body.password, salt, function(err, hash) {
+                            if(err){
+                                throw new Error(err);
+                                return;
+                            }
+                            var confirm_hash = randomstring.generate({
+                                length: 30,
+                                charset: 'alphanumeric'
+                            });
+                            mUsers.createUser({
+                                ...req.body, 
+                                password: hash,
+                                confirm_hash: confirm_hash,
+                                confirmed: false
+                            });
+                            sgMail.setApiKey(config.getConfig('mailer').key);
+                            const msg = {
+                                to: req.body.email,
+                                from: 'bojang@gmail.com',
+                                subject: 'Thanks for registering',
+                                text: 'Thanks for registering',
+                                html: `<a href="http://localhost:8001/api/v1/confirm/${confirm_hash}">Click here to confirm your account</a>`,
+                            };
+                            sgMail.send(msg);
+                            return;
+                        });
                     });
-                    mUsers.createUser({
-                        ...req.body, 
-                        password: hash,
-                        confirm_hash: confirm_hash,
-                        confirmed: false
-                    });
-                    // Store hash in your password DB.
-            sgMail.setApiKey(config.getConfig('mailer').key);
-            const msg = {
-                to: req.body.email,
-                from: 'sgorgevik@gmail.com',
-                subject: 'thanks for registering',
-                text: 'thanks for registering',
-                html: `<a href="http://localhost:8001/api/v1/confirm/${confirm_hash}">Thanks for registering</a>`
-            };
-            sgMail.send(msg);
-            return;
-        }); 
-    });
-    }else {
-        throw new Error('Validation failed');
-    }
-})
+                } else {
+                    throw new Error('Bad Request - User Exists');
+                }
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+        } else {
+            throw new Error('Validation failed');
+        }
+    })
     .then(() => {
-        return res.status(201).send('ok')
-        
+        return res.status(201).send('ok');
     })
     .catch(err => {
-        console.log(err)
-        return res.status(500).send(v.errors)
+        console.log(err);
+        return res.status(500).send(v.errors);
     });
 }
+
 const login = (req, res) => {
     mUsers.getUserPasswordByEmail(req.body.email)
     .then((data) => {
         bcrypt.compare(req.body.password, data.password, function(err, rez) {
             if(err){
-                return res.status(500).send('could not compare password');
+                return res.status(500).send('Could not compare password');
             }
             if(rez){
-                // return res.status(200).send('ok');
                 var tokenData = {
                     id: data._id,
                     full_name: `${data.first_name} ${data.last_name}`,
@@ -87,17 +95,20 @@ const login = (req, res) => {
 };
 
 const renew = (req, res) => {
-    return res.status(200).send(req.user)
+    return res.status(200).send(req.user.id);
 }
+
 const resetLink = (req, res) => {
-    return res.status(200).send('ok')
+    return res.status(200).send('ok');
 }
+
 const resetPassword = (req, res) => {
-    return res.status(200).send('ok')
-}   
+    return res.status(200).send('ok');
+}
+
 const changePassword = (req, res) => {
-    return res.status(200).send('ok')
-}  
+    return res.status(200).send('ok');
+}
 
 const confirm = (req, res) => {
     // koga nekoj kje klikne na 
@@ -113,10 +124,9 @@ const confirm = (req, res) => {
         return res.status(200).send('ok');
     })
     .catch((err) => {
-        return res.status(500).send('Internal server error');
+        return res.status(500).send('Internal Server Error');
     })
 }
-
 
 module.exports = {
     register,
